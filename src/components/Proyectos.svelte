cc376dc3423de3106df07d560cba97ab7e3f4074
juanfuc <juanfuc@gmail.com>
2023-11-01 20:52:23 -0500

    primera version pagina principal

---
+++ src/components/Proyectos.svelte
@@ -0,0 +1,70 @@
+<script>
+// let proyectos = [
+//     {nombre: "Mapa de la Bibliografía del Programa de Historia", descripcion: "", imagen: "assets/imgs/1.png", width: "500px", marginLeft: "45%"},
+//     {nombre: "Mapas de Bogotá", descripcion: "...", imagen: "assets/imgs/2.png", width: "300px", marginLeft: "45%"},
+//     {nombre: "Proyectos de Humanidades Digitales", descripcion: "...", imagen: "assets/imgs/3.png", width: "400px", marginLeft: "50%"},
+//     {nombre: "Momentos de Fútbol y Política", descripcion: "...", imagen: "assets/imgs/4.png", width: "550px", marginLeft: "35%"}
+// ];
+
+
+let proyectos = [
+    {nombre: "Mapa de la Bibliografía del Programa de Historia", descripcion: "", imagen: "assets/imgs/1.png"},
+    {nombre: "Mapas de Bogotá", descripcion: "...", imagen: "assets/imgs/2.png"},
+    {nombre: "Proyectos de Humanidades Digitales", descripcion: "...", imagen: "assets/imgs/3.png"},
+    {nombre: "Momentos de Fútbol y Política", descripcion: "...", imagen: "assets/imgs/4.png"}
+];
+</script>
+
+<div class='contenedor-proyectos'>
+
+        {#each proyectos as proyecto, i}
+        <div class="tarjeta-proyecto" style="width: {proyecto.width}; margin-left: {proyecto.marginLeft};">
+
+            <div class="img-proyecto">
+                <img src="{proyecto.imagen}" alt="imagen del proyecto">
+            </div>
+        </div>
+        {/each}
+</div>
+
+<style>
+    .contenedor-proyectos {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        padding: 12px;
+        margin-left: 10%;
+        margin-right: 10%;
+        scroll-behavior: smooth;
+    }
+
+    .tarjeta-proyecto {
+        flex: 1 1 45%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 2% 2%;
+    }
+
+    .textos {
+        display: flex;
+        flex-direction: column;
+    }
+    .tarjeta-proyecto .img-proyecto img {
+        width: 100%;
+        max-width: 100%;
+        height: auto;
+        border-radius: 100px;
+        box-shadow: 0 0 16px #333;
+        transition: all 1s ease;
+        cursor: pointer;
+    }
+    .tarjeta-proyecto .img-proyecto img:hover {
+            transform: translate3d(0px, -10.405px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(1deg) skew(0deg);
+            width: 103%;
+        max-width: 103%;
+
+        }
+
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,80 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Proyectos from '../components/Proyectos.svelte'
+
+
+</script>
+
+<div class="contenedor-general">
+
+    <header>
+        <div class="titulo"><h1>Proyectos HD</h1><h1>historiaJaveriana</h1></div>
+        <div class="nav"><a href="#acerca"><h3>Acerca</h3></a></div>
+    </header>
+
+    <div class="contenido">
+
+        <div class="proyecto">
+            <Proyectos/>
+        </div>
+
+    </div>
+
+</div>
+
+<style>
+     .contenedor-general {
+            display: flex;
+            flex-direction: column;
+         
+ 
+        }
+
+        header {
+            display: flex;
+            justify-content: space-around;
+            align-items: baseline;
+            background-color: #fffdfc;
+            width: 100%;
+            padding: 12px;
+        }
+
+        .titulo {
+            font-size: 50px;
+            font-family: 'Public Sans', sans-serif;
+            color: #000;
+        }
+
+        .nav {
+            display: flex;
+            flex-direction: column;
+            justify-content:baseline;
+            font-size: .7em;
+            font-family: 'Public Sans', sans-serif;
+            text-align: center;
+            max-width: 100%;
+            font-weight: 800;
+        }
+
+        .nav a {
+            background-color: #f96400;
+            font-family: 'Public Sans', sans-serif;
+            width: 7vw;
+            height: 3vw;
+            text-decoration: none;
+            color: #fffdfc;
+            font-size: 1.5em;
+            border-radius: 100px;
+            cursor: pointer;
+            border-left: 1px solid rgba(0, 0, 0, 0);
+            transform-style: preserve-3d;
+        }
+
+        .nav a:hover {
+            transform: translate3d(0px, -10.405px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(8deg) rotateZ(9.11765deg) skew(0deg);
+            width: 7.5vw;
+            height: 3.5vw;
+            transition: all 1s ease;
+        }
+
+        
+</style>
